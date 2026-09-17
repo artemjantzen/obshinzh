a0119f19aeb5791c3f1767db357032643c3aff6e
artemjantzen <66843609+artemjantzen@users.noreply.github.com>
2026-09-17 16:51:48 +0300

    17/9/ task

---
+++ 04-pwm.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+led = 26
+button = 13
+foto = 6
+state = 0
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(foto, GPIO.IN)
+
+pwm = GPIO.PWM(led, 200)
+duty = 0.0
+pwm.start(duty)
+
+while True:
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.05)
+    
+    duty += 1.0
+    if duty > 100.0:
+        duty = 0.0
