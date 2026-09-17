7879d70ff6770edba73e19d231601eafadca7300
artemjnt <artyantsen@yahoo.com>
2026-09-17 17:25:36 +0300

    update to 07--.py

--- 06-jumper-button.py
+++ 06-jumper-button.py
@@ -16,16 +16,5 @@ GPIO.setup(down, GPIO.IN)
 GPIO.output(leds, 0)
 
 while True:
-    if GPIO.input(up):
-        print('up')
-        if (num < 255):
-            num = num + 1
-            print(num, dec2bin(num))
-            GPIO.output(leds, dec2bin(num))
-    if GPIO.input(down):
-        print('down')
-        if (num > 255):
-            num = num - 1
-            print(num, dec2bin(num))
-            GPIO.output(leds, dec2bin(num))
+    print(GPIO.input(up));
     time.sleep(0.1)
--- 07-jumper-button-upgrade.py
+++ 07-jumper-button-upgrade.py
@@ -8,6 +8,7 @@ GPIO.setmode(GPIO.BCM)
 #led = 26
 button = 13
 num = 0
+up, down = 9, 10
 leds = [24, 22, 23, 27, 17, 25, 12, 16]
 GPIO.setup(leds, GPIO.OUT)
 GPIO.setup(up, GPIO.IN)
@@ -16,15 +17,15 @@ GPIO.output(leds, 0)
 
 while True:
     if GPIO.input(up) and GPIO.input(down):
-        num = 255
-    if GPIO.input(up) and not GPIO.input(down):
-        if (num < 7):
+        num = 256
+    elif GPIO.input(up) and not GPIO.input(down):
+        if (num < 255):
             num = num + 1
             print(num, dec2bin(num))
             GPIO.output(leds, dec2bin(num))
-    if GPIO.input(down) and not GPIO.input(up):
+    elif GPIO.input(down) and not GPIO.input(up):
         if (num > 0):
             num = num - 1
             print(num, dec2bin(num))
             GPIO.output(leds, dec2bin(num))
-    time.sleep(0.2)
+    time.sleep(0.1)
