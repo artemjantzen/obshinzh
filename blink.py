f8b63bb026373e41482cccf9c297f2aaefa48622
artemjantzen <66843609+artemjantzen@users.noreply.github.com>
2026-09-17 16:05:46 +0300

    17/9/ task

---
+++ blink.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+led = 26
+GPIO.setup(led, GPIO.OUT)
+
+while True:
+    GPIO.output(led, 0)
+    time.sleep(1)
+    GPIO.output(led, 1)
+    time.sleep(1)
