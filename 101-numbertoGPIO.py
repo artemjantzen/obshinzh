d3ae5d5769b385c399586e225c76c1a4d99a9cbf
artemjantzen <66843609+artemjantzen@users.noreply.github.com>
2026-09-24 16:04:26 +0300

    24/9

---
+++ 101-numbertoGPIO.py
@@ -0,0 +1,20 @@
+def voltage_to_number(voltage):
+    if not (0.0 <= voltage <= dynamic_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
+        print("Устанавлниваем 0.0 В")
+        return 0
+
+    return int(voltage / dynamic_range * 255)
+
+try:
+    while True:
+        try:
+            voltage = float(input("Введите напряжение в Вольтах: "))
+            number = voltage_to_number(voltage)
+    
+        except ValueError:
+            print("Вы ввели не число. Попробуйте ещё раз\n")
+
+finally:
+    GPIO.output(bin(number)[2:], 0)
+    GPIO.cleanup()
